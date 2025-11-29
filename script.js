8cfc263b5b6c5aea64a1ad2e7981f4f6b311c5c6
hxnq <76233782+isKuyo@users.noreply.github.com>
2025-11-29 10:40:34 -0300

    Enhance Discord status messages and iframe layout
    Update status display for Discord activity and improve iframe styling.

--- script.js
+++ script.js
@@ -107,11 +107,14 @@ async function fetchDiscordPresence() {
           // No activity - show status message based on discord_status
           if (discord_status === 'offline') {
             activityEl.innerHTML = '<span class="offline-message">Offline</span>';
+          } else if (discord_status === 'online') {
+            // Para status online, mostrar apenas "Online"
+            activityEl.innerHTML = '<span class="status-online">Online</span>';
           } else {
-            // Usar a classe correspondente ao status
+            // Para outros status (idle, dnd), mostrar "Online (Status)"
             const statusClass = `status-${discord_status}`;
             const statusText = discord_status.charAt(0).toUpperCase() + discord_status.slice(1); // Capitalizar
-            activityEl.innerHTML = `<span class="${statusClass}">${statusText}</span>`;
+            activityEl.innerHTML = `<span class="${statusClass}">Online (${statusText})</span>`;
           }
         }
       }
@@ -821,12 +824,11 @@ function renderWorks(filter = '') {
       const streamableId = work.video.split('/').pop();
       videoContent = `
         <div class="streamable-embed">
-          <iframe
-            src="https://streamable.com/e/${streamableId}?loop=0"
-            frameborder="0"
-            scrolling="no"
-            allowfullscreen
-            style="width:100%;height:100%;"
+          <iframe 
+            allow="fullscreen" 
+            allowfullscreen 
+            src="https://streamable.com/e/${streamableId}?loop=0" 
+            style="border:none; width:100%; height:100%; position:absolute; left:0px; top:0px; overflow:hidden;"
           ></iframe>
         </div>
       `;
